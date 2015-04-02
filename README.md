a38e9eaa4a92fcd9458388d338d275d87d00dbf9
Fengda HUANG <h@phodal.com>
2015-04-02 09:13:39 +0800

    finish function

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# showdown-star
+#Showdown Star Plugin
+
+##Usage
+
+    var converter = new Showdown.converter({ extensions: ['star'] });
+    var result = converter.makeHtml('<level1> <br/> <level1.5> <br/> <level3> <br/> <level3.5>');
+    document.getElementById('show').innerHTML = result;
+    
+##ScreenShot    
+
+![ScreenShot](./screenshot.jpg)
---
+++ src/star.js
@@ -0,0 +1,30 @@
+//
+//  A showdown extension to add star for Resume
+//  hints to showdown's HTML output.
+//
+
+(function(){
+
+    var star = function(converter) {
+        return [
+            { type: 'output', filter: function(source){
+                return source.replace(/(<level([0-9]*\.?[0-9]+)>)/gi, function(match, isLevel) {
+                    var level = match.match(/<level([0-9]*\.?[0-9]+)>/i)[1];
+                    level = level.replace(".", "-");
+
+                    if (isLevel) {
+                        return '<i class="star-icon icon-star star-' + level + '"></i>';
+                    } else {
+                        return '<i class="star-icon icon-star star-1"></i>';
+                    }
+                });
+            }}
+        ];
+    };
+
+    // Client-side export
+    if (typeof window !== 'undefined' && window.Showdown && window.Showdown.extensions) { window.Showdown.extensions.star = star; }
+    // Server-side export
+    if (typeof module !== 'undefined') module.exports = star;
+
+}());
